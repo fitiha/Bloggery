c4b5a61891209dd6f410d00b02e5a71bdd89e8dd
fitiha <fitihanegst1995@gmail.com>
2024-04-09 00:54:45 +0300

    better responsive UI

--- Frontend/src/pages/GettingStarted.jsx
+++ Frontend/src/pages/GettingStarted.jsx
@@ -21,18 +21,18 @@ const GettingStarted = () => {
                 <div className='flex flex-col justify-center items-center space-y-4'>
                     <h1 className="text-5xl font-bold text-center">Connect, Create, Share</h1>
                     <p className="text-xl text-center">Join the vibrant <span className='text-orange-600'>Bloggery</span> community and bring your stories to life.</p>
-                    <Link to="/register" className="bg-gray-800 text-white px-9 py-5 rounded-lg font-['Cinzel'] font-medium text-2xl hover:bg-orange-800 transition-colors">Create Your First Post</Link>
+                    <Link to="/register" className="bg-gray-800 text-white px-9 py-5 rounded-lg font-['Cinzel'] font-medium text-lg lg:text-2xl hover:bg-orange-800 transition-colors">Create Your First Post</Link>
 
                 </div>
             </div>
         </div>
         <hr />
 
-        <div className="flex flex-col items-center mt-8">
-            <h1 className="text-8xl font-['Georgia']">How it works?</h1><br />
+        <div className="flex flex-col items-center my-8">
+            <h1 className="text-5xl lg:text-8xl text-orange-700 font-['Georgia']">How it works</h1><br />
             <iframe
                 className="w-11/12 md:w-3/4 lg:w-1/2 xl:w-1/2 h-64 md:h-96"
-                src="https://www.youtube.com/embed/1he_Izj8AHc"
+                src="https://www.youtube.com/embed/kyjpj1RrsyI"
                 title="Bloggery guide"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen>
